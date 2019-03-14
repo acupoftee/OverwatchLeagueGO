4375f372d8b2389f9150e5c3e9ce1a86628fbc83
acupoftee <bdiakaba@uccs.edu>
2019-03-14 14:58:04 -0600

    adding table helper

---
+++ src/utils/BasicTable.js
@@ -0,0 +1,25 @@
+const Table = require('cli-table3');
+module.exports = {
+    TABLE: function() {
+        return new Table({
+            chars: {
+                'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+                , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+                , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+                , 'right': '║', 'right-mid': '╢', 'middle': '│'
+            },
+            style: { "padding-left": 0, "padding-right": 0, head: [], border: [] }
+        });
+    },
+    TABLE_HEADERS: function(headers) {
+        return new Table({
+            chars: {
+                'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
+                , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
+                , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
+                , 'right': '║', 'right-mid': '╢', 'middle': '│'
+            },
+            head: headers, style: { "padding-left": 0, "padding-right": 0, head: [], border: [] }
+        })
+    }
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ const Logger = require('./Logger');
 const JsonUtil = require('./JsonUtil');
 const EmojiUtil = require('./EmojiUtil');
 const OwlUtil = require('./OwlUtil');
+const BasicTable = require('./BasicTable');
 
 module.exports = {
     Logger,
     JsonUtil,
     EmojiUtil,
-    OwlUtil
+    OwlUtil,
+    BasicTable
 }
