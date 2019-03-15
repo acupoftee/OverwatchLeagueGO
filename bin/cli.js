9bf2d6e83fc807c1c4405a3a5684e1649040c388
acupoftee <bdiakaba@uccs.edu>
2019-03-14 19:57:41 -0600

    adding webpack config

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,7 @@
-	//!/usr/bin/env node;
+#!/usr/bin/env node
+if (process.env.NODE_ENV === 'development') {
+	require('@babel/register');
 	require('../src/cli');
+  } else {
+	require('../lib/cli');
+  }
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node',
+    node: {
+        __dirname: false,
+    },
+    externals: [nodeExternals()],
+    entry: {
+        cli: path.join(__dirname, 'src', 'cli.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'lib'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.join(__dirname, 'src', 'utils'),
+                to: path.join(__dirname, 'lib'),
+            },
+        ]),
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin()],
+    },
+};
